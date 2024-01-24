d214bbbb72787b5829699c2619e5b38ecedd1a92
gaeun <0107162a@gmail.com>
2024-01-24 14:13:47 +0900

    hello, java, korea 추가

--- Ch01_HelloWorld/src/ch01_hello/Hello.java
+++ Ch01_HelloWorld/src/ch01_hello/Hello.java
@@ -2,6 +2,8 @@ package ch01_hello;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("HelloWorld");
+		System.out.println("hello, java");
+		System.out.println("hello, korea");
 	}
 }
