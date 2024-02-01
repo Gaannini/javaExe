58996ded7144d2d504b7b8b4e7a6d60bf467b735
Gaannini <0107162a@gmail.com>
2024-02-01 10:25:26 +0900

    20240131_1번문제발표

--- Ch09_Extends/src/_07resolve2/Triangle.java
+++ Ch09_Extends/src/_07resolve2/Triangle.java
@@ -4,18 +4,17 @@ public class Triangle {
 	static int high;
 	static int base;
 
-	public Triangle(int high, int base) {
+	public Triangle(int high, int base) {// 매개변수를 가진 생성자
 		this.high = high;
 		this.base = base;
-		System.out.println("삼각형 높이 : " + high);
-		System.out.println("삼각형 밑변 : " + base);
+		Triangle_show();
 	}
 
 	// 밑변과 높이를 모두 변경하는 메소드
 	public void Change_All(int change_high, int change_base) {
 		this.high = change_high;
 		this.base = change_base;
-		System.out.println("삼각형의 높이를 " + high + "(으)로 변경하고 밑변 " + base + "(으)로 변경하였습니다.");
+		System.out.println("삼각형의 높이를 " + high + "(으)로 변경하고 밑변을 " + base + "(으)로 변경하였습니다.");
 	}
 
 	// 높이만 변경하는 메소드
@@ -30,9 +29,13 @@ public class Triangle {
 		System.out.println("삼각형의 밑변 " + base + "(으)로 변경하였습니다.");
 	}
 
-	// 삼각형의 넓이를 반환하는 메소
+	public void Triangle_show() {
+		System.out.println("삼각형 높이 : " + high);
+		System.out.println("삼각형 밑변 : " + base);
+	}
+
+	// 삼각형의 넓이를 반환하는 메소드
 	public double TriangleArea() {
 		return ((double) high * (double) base) / 2;
 	}
-
 }
